48efc75ccd66e2678d63db2dba583ad731164293
RanjaniPrasad <101932134+RanjaniPrasad@users.noreply.github.com>
2022-03-23 23:16:53 +0530

    Add files via upload

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ranjani prasad\source\repos\saiparayan\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
